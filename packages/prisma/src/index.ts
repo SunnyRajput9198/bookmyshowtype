940800397891b78a6961dcaa1a20f5cb994379bd
sunny <rajputsny2@gmail.com>
2025-10-06 04:03:05 +0530

    completed the basic application logic

---
+++ packages/prisma/src/index.ts
@@ -0,0 +1,4 @@
+import { PrismaClient } from "@prisma/client";
+// if {prismaclient} me error aaye to ouput path hatso scema.prisma se yah phir vhis se importb kro
+
+export const client = new PrismaClient();
